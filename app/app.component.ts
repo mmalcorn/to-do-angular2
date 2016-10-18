13c62ff52ef38ff38d144d340ce6b4fb89b11c6b
zach <zach>
2016-10-18 11:18:47 -0400

    use the skeleton to initialize the to do list for angular 2

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>Angular 2 To Do</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
