cb8c74fa6618451c5ca376a878860ff9b5634740
Lisa MacCarrigan, Meredith Alcorn and Zach Hedgepeth <meredith@>
2016-10-18 15:47:29 -0400

    update app.component.ts to store data with a model, store instances of that model in a component, and display the data in a view by using the controller/component class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 
+// view
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>Angular 2 To Do</h1>
+    <h1>Angular 2 To Do App</h1>
+    <h3 *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
   </div>
   `
 })
 
+// controller
 export class AppComponent {
+  public tasks: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Jeet Kun Do.", 1),
+      new Task("Rewatch all the Twilight movies then blow my brains out.", 2),
+      new Task("Do the laundry once it gets too piled up, and I cant think because of it.", 3)
+  ];
+}
 
+// model
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {  }
 }
