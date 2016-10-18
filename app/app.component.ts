2979ee8e836f784241df114bf849f1220de16377
Lisa MacCarrigan, Meredith Alcorn and Zach Hedgepeth <lisa@>
2016-10-18 16:10:58 -0400

    update app.component.ts to make tasks clickable and to display task descriptions

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Angular 2 To Do App</h1>
-    <h3 *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <div>
+      <h1>Edit Task</h1>
+      <p>Description: {{ selectedTask.description }}</p>
+      <p>Task ID: {{ selectedTask.id }}</p>
+    </div>
   </div>
   `
 })
@@ -19,6 +24,10 @@ export class AppComponent {
       new Task("Rewatch all the Twilight movies then blow my brains out.", 2),
       new Task("Do the laundry once it gets too piled up, and I cant think because of it.", 3)
   ];
+  selectedTask: Task = this.tasks[0];
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
+  }
 }
 
 // model
